a91e4388963a82d58474fa07d8257a122bb3c413
hola12z <nebula3477@gmail.com>
2025-03-19 00:47:56 +0530

    oh uh nothing

--- random.cpp
+++ random.cpp
@@ -3,10 +3,32 @@ using namespace std;
 typedef long long ll;
 #define endl "\n"
 
-int main() {
-    volatile int x = 3;
-    x = 8;
-    cout << ++x << endl;
-    
+vector<int> twosum(const vector<int> &vect, int target)
+{
+    int n = vect.size();
+    map<int, int> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        int comp = target - vect[i];
+
+        if (mp.count(comp))
+        {
+            return {mp[comp],i};
+        }
+
+        mp[vect[i]] = i;
+    }
+    return {};
+}
+
+int main()
+{
+    vector<int> v = twosum({2, 3, 5, 8, 9}, 17);
+
+    for (auto &el : v)
+    {
+        cout << el << " ";
+    }
     return 0;
 }
