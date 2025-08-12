3c9705f555c0c6f9a0e8f5c55c8380feb1a2e76f
himanshu <118502619+himaenshuu@users.noreply.github.com>
2025-08-12 17:53:15 +0530

    c

--- dp/frog_jump_k.cpp
+++ dp/frog_jump_k.cpp
@@ -3,8 +3,44 @@ using namespace std;
 typedef long long ll;
 #define endl "\n"
 
-int main() {
-    cout << "" << endl;
-    
+// frog_jump problem with k distances
+class Solution
+{
+public:
+    int fun(vector<int> &heights, int k)
+    {
+        int n = heights.size();
+
+        vector<int> dp(n, -1);
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j <= i && j <= k; j++)
+            {
+                dp[i] = min(dp[i], dp[i - j] + abs(heights[i] - heights[i - j]));
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int k;
+    cin >> k;
+    Solution obj;
+    cout << obj.fun(v, k) << endl;
+
     return 0;
 }
