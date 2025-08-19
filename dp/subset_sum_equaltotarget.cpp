bdd1ef90f5fbfab325d98b684b3cfd5e418b4358
himanshu <118502619+himaenshuu@users.noreply.github.com>
2025-08-19 13:17:45 +0530

    c

---
+++ dp/subset_sum_equaltotarget.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl "\n"
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> vect(n);
+
+    return 0;
+}
+
