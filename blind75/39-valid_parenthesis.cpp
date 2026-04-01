976ea06573f06b1994b35c74aa87258837f997a5
Himaenhuu <himanshusingh26.2.2004@gmail.com>
2026-04-01 21:54:38 +0530

    Refactor isValid function in valid_parenthesis.cpp for improved readability and structure

--- blind75/39-valid_parenthesis.cpp
+++ blind75/39-valid_parenthesis.cpp
@@ -6,8 +6,37 @@ typedef long long ll;
 
 class Solution
 {
-    public:
-    
+public:
+    bool isValid(string s)
+    {
+
+        unordered_map<char, char> mp = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
+
+        stack<char> st;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++)
+        {
+
+            if (mp.find(s[i]) != mp.end())
+            {
+                st.push(s[i]);
+                continue;
+            }
+            if (st.empty())
+            {
+                return false;
+            }
+
+            char ch = st.top();
+            st.pop();
+            if (s[i] == mp[ch])
+                continue;
+            else
+                return false;
+        }
+        return st.empty() ? true : false;
+    }
 };
 
 int main()
