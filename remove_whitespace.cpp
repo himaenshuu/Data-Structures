e3cb0d64c1e1b9cd5d500dfef1cd89aa8dba43f1
Himaenhuu <himanshusingh26.2.2004@gmail.com>
2026-04-23 22:22:41 +0530

    Add initial implementation of whitespace removal program

---
+++ remove_whitespace.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void removewhitespace(string s){
+    
+}
+
+int main()
+{
+
+    int num;
+    cin >> num;
+
+    string str[num];
+//enter the string array
+    for (int i = 0; i < num; i++)
+    {
+        getline(cin, str[i]);
+    }
+//remove the whitespace
+    //search for the substring
+    return 0;
+}
